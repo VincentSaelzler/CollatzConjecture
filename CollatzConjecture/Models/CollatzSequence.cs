30706527f4d19206e6b28d6fe266f45ee02050c1
vsaelzler <vincent.saelzler@gmail.com>
2017-07-11 15:01:05 -0400

    convert recursive AddNext function to while loop based approach

--- CollatzConjecture/Models/CollatzSequence.cs
+++ CollatzConjecture/Models/CollatzSequence.cs
@@ -11,6 +11,8 @@ namespace CollatzConjecture.Models
         public int InitialValue { get; private set; }
         public int? RepeatedValIndex { get; private set; }
         public bool TendsToInfinity { get; private set; }
+        public int EvenExp { get; private set; }
+        public int OddExp { get; private set; }
         public int? TotalStoppingTime
         {
             get
@@ -45,51 +47,69 @@ namespace CollatzConjecture.Models
         }
         public IDictionary<int, int> Steps { get; set; } //IDictionary<stepVal, stepIndex>
         #endregion
-        public CollatzSequence(int initialValue, int eventExp, int oddExp)
+        public CollatzSequence(int initialValue, int evenExp, int oddExp)
         {
+            //constructor parameters to properties
             InitialValue = initialValue;
             Steps = new Dictionary<int, int> { { initialValue, 0 } };
-            AddNext(initialValue, eventExp, oddExp, this);
-        }
-        private void AddNext(int inValue, int evenExp, int oddExp, CollatzSequence collatzSequence)
-        {
-            if (inValue != 1)
-            {
-                //calculate next value
-                int nextValue;
-                if (inValue % 2 == 0) //number is even
-                {
-                    nextValue = inValue / evenExp;
-                }
-                else
-                {
-                    nextValue = inValue * oddExp + 1;
-                }
+            EvenExp = evenExp;
+            OddExp = oddExp;
 
-                //assuming that values that roll over int.maxvalue are tending to infinity
-                if (nextValue < 0)
+            //containers
+            int inValue = initialValue;
+            int? nextValue = null;
+
+            //generate step data
+            while (TryGetNext(inValue, out nextValue))
+            {
+                Steps.Add((int)nextValue, Steps.Count);
+                inValue = (int)nextValue;
+            }
+        }
+        public long CalcNext(int inValue)
+        {
+            //calculate next value
+            if (inValue % 2 == 0) //number is even
+            {
+                return inValue / EvenExp;
+            }
+            else
+            {
+                return inValue * OddExp + 1;
+            }
+        }
+        private bool TryGetNext(int inValue, out int? nextValue)
+        {
+            //already at end state
+            if (inValue == 1)
+            {
+                nextValue = null;
+                return false;
+            }
+            else
+            {
+                //calculate as potentially valid next value
+                long potentialNextValue = CalcNext(inValue);
+
+                //tending to infinity
+                if (potentialNextValue > int.MaxValue || potentialNextValue < int.MinValue)
                 {
                     TendsToInfinity = true;
+                    nextValue = null;
+                    return false;
                 }
+                //repeating loop
+                else if (Steps.ContainsKey((int)potentialNextValue))
+                {
+                    RepeatedValIndex = Steps[(int)potentialNextValue];
+                    nextValue = null;
+                    return false;
+                }
+                //valid
                 else
                 {
-                    //take actions
-                    if (nextValue == 1)
-                    {
-                        collatzSequence.Steps.Add(nextValue, collatzSequence.Steps.Count()); //TODO: check that the count is taken BEFORE the .Add call
-                    }
-                    else
-                    {
-                        if (collatzSequence.Steps.ContainsKey(nextValue))
-                        {
-                            collatzSequence.RepeatedValIndex = Steps[nextValue];
-                        }
-                        else
-                        {
-                            collatzSequence.Steps.Add(nextValue, collatzSequence.Steps.Count());
-                            AddNext(nextValue, evenExp, oddExp, collatzSequence);
-                        }
-                    }
+                    nextValue = (int)potentialNextValue;
+                    return true;
                 }
             }
         }
